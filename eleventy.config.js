dab1878d84b08d00bb16e7fe28f7dd31f25a7195
Manuel Berganza <manuelberganza.dev@gmail.com>
2024-11-23 17:40:16 -0600

    CV actualizada y modifcamos la config de 11ty para que copie siempre que se compile el archivo PDF de assets

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,7 @@ const { EleventyHtmlBasePlugin } = require('@11ty/eleventy');
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("code/img");
     eleventyConfig.addPassthroughCopy("code/js");
+    eleventyConfig.addPassthroughCopy("code/assets");
 
     eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 
