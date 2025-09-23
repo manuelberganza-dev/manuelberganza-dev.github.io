327d340a5b907175f2f6d63653945d226b05e0ca
The Blake BGZ <manuelberganza.dev@gmail.com>
2025-09-22 20:13:31 -0600

    Adaptacion de los textos en el home, descripcion, links y navegacion

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -40,20 +40,16 @@ export type SiteConfig = {
 
 const siteConfig: SiteConfig = {
     website: 'https://manuelberganza-dev.github.io',
-    title: 'Dante',
-    subtitle: 'Minimal Astro.js theme',
-    description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
-    image: {
-        src: '/dante-preview.jpg',
-        alt: 'Dante - Astro.js and Tailwind CSS theme'
-    },
+    title: 'Manuel Berganza',
+    subtitle: 'Portafolio y Blog de un Desarrollador Backend',
+    description: 'Manuel Berganza programador backend Java, Spring y Node.js. Trabajo con microservicios, APIs REST y bases de datos MySQL. También tengo experiencia en TypeScript, JavaScript, Docker, Linux y Git. Bienvenido a mi espacio donde comparto proyectos, aprendizajes y recursos de programación.',
     headerNavLinks: [
         {
-            text: 'Home',
+            text: 'Inicio',
             href: '/'
         },
         {
-            text: 'Projects',
+            text: 'Proyectos',
             href: '/projects'
         },
         {
@@ -67,54 +63,29 @@ const siteConfig: SiteConfig = {
     ],
     footerNavLinks: [
         {
-            text: 'About',
+            text: 'Sobre mí',
             href: '/about'
         },
         {
-            text: 'Contact',
+            text: 'Contacto',
             href: '/contact'
         },
-        {
-            text: 'Terms',
-            href: '/terms'
-        },
-        {
-            text: 'Download theme',
-            href: 'https://github.com/JustGoodUI/dante-astro-theme'
-        }
     ],
     socialLinks: [
         {
-            text: 'Dribbble',
-            href: 'https://dribbble.com/'
+            text: 'LinkedIn',
+            href: 'https://www.linkedin.com/in/manuel-berganza/'
         },
-        {
-            text: 'Instagram',
-            href: 'https://instagram.com/'
-        },
-        {
-            text: 'X/Twitter',
-            href: 'https://twitter.com/'
-        }
     ],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
-        image: {
-            src: '/hero.jpeg',
-            alt: 'A person sitting at a desk in front of a computer'
-        },
+        title: 'Bienvenidos a mi Blog!',
+        text: "**Manuel Berganza**, programador Java, Spring y Node.js, especializado en microservicios y APIs REST. También manejo MySQL, TypeScript, JavaScript, Docker, Linux y Git. Me apasiona crear soluciones escalables y eficientes, Puedes explorar algunos de mis trabajos en <a href='https://github.com/manuelberganza-dev'>GitHub</a>",
         actions: [
             {
-                text: 'Get in Touch',
+                text: 'Contáctame',
                 href: '/contact'
             }
         ]
-    },
-    subscribe: {
-        title: 'Subscribe to Dante Newsletter',
-        text: 'One update per week. All the latest posts directly in your inbox.',
-        formUrl: '#'
     },
     postsPerPage: 8,
     projectsPerPage: 8
